9e08ac6efb8107dda3f60d97c55c90c3049367d9
SunnyTamang <spikestamang@gmail.com>
2022-01-14 21:29:50 +0530

    Signup page route added on click

--- main/routes.py
+++ main/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('login.html')
+
+@main.route('/sign-up')
+def signup():
+    return render_template('signup.html')
