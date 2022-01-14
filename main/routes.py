152a5dcb7dd423ac2b7137e30f97a7ddc5a5348c
SunnyTamang <spikestamang@gmail.com>
2022-01-14 21:35:58 +0530

    Signup page added

--- main/routes.py
+++ main/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def home():
+def login():
     return render_template('login.html')
 
 @main.route('/sign-up')
