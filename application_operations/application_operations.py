b444bcfda3df19bb93daf8e68444106e90f72df8
Sunny Tamang <spikestamang@gmail.com>
2022-05-23 00:26:26 +0530

    missing values page completed

--- application_operations/application_operations.py
+++ application_operations/application_operations.py
@@ -1,6 +1,10 @@
 # It creates a class called application_operations.
 from multiprocessing.reduction import duplicate
-from flask import Blueprint, redirect,render_template,flash,request, session, url_for, Response
+from turtle import width
+from flask import Blueprint, redirect,render_template,flash,request, session, url_for, Response,send_file
+from matplotlib import cm
+import matplotlib
+matplotlib.use('Agg')
 import pandas  as pd
 import numpy as np
 from pyparsing import col
@@ -9,9 +13,12 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
+
+# fig,ax  = plt.subplots(figsize=(6,6))
 
 class application_operations:
 
@@ -26,7 +33,13 @@ class application_operations:
       self.filename=filename
       self.dataset = pd.read_csv(filename)
 
+   def return_dataframe(self,filename):
+      self.filename=filename
+      self.dataset = pd.read_csv(filename)
+      return self.dataset
+
    def shape(self):
+      print(self.dataset.shape, session['filename'])
       return self.dataset.shape
 
    
@@ -71,13 +84,84 @@ class application_operations:
       return  describe_columns, describe
 
 
-   def plot_png(self):
+   # def plot_png(self):
       
-      fig = Figure()
-      axis = fig.add_subplot(1, 1, 1)
-      xs = np.random.rand(100)
-      ys = np.random.rand(100)
-      axis.plot(xs, ys)
-      output = io.BytesIO()
-      FigureCanvas(fig).print_png(output)
-      return Response(output.getvalue(), mimetype='image/png')
+   #    fig = Figure()
+   #    axis = fig.add_subplot(1, 1, 1)
+   #    xs = np.random.rand(100)
+   #    ys = np.random.rand(100)
+   #    axis.plot(xs, ys)
+   #    output = io.BytesIO()
+   #    FigureCanvas(fig).print_png(output)
+   #    return Response(output.getvalue(), mimetype='image/png')
+
+   def pearson_correlation(self):
+      try:
+         data1 = self.dataset
+         
+         fig = plt.figure(figsize=(8,8))
+         
+         plt.title(f"Heatmap of Pearson's Correlation", fontsize = 20) # title with fontsize 20
+         # plt.tight_layout()
+         sns.heatmap(data1.corr('pearson'), cmap="Blues", annot=True)
+         canvas = FigureCanvas(fig)
+         img1= io.BytesIO()
+         fig.savefig(img1)
+         # plt.cla()
+         # plt.clf()
+         img1.seek(0)
+         
+         return send_file(img1, mimetype='image/png')
+      except Exception as e:
+         print(e)
+      
+   def spearman_correlation(self):
+      data2 = self.dataset
+      
+      fig2 = plt.figure(figsize=(8,8))
+      plt.title(f"Heatmap of Spearman's Correlation", fontsize = 20) # title with fontsize 20
+      # plt.tight_layout()
+      sns.heatmap(data2.corr('spearman'), cmap="Blues", annot=True)
+      canvas = FigureCanvas(fig2)
+      img2= io.BytesIO()
+      fig2.savefig(img2)
+      # plt.cla()
+      img2.seek(0)
+      return send_file(img2, mimetype='image/png')
+   
+   def kendall_correlation(self):
+      data3 = self.dataset
+      
+      fig3 = plt.figure(figsize=(8,8))
+      plt.title(f"Heatmap of Kendall's Correlation", fontsize = 20) # title with fontsize 20
+      # plt.tight_layout()
+      sns.heatmap(data3.corr('kendall'), cmap="Blues", annot=True)
+      canvas = FigureCanvas(fig3)
+      img3= io.BytesIO()
+      fig3.savefig(img3)
+      # plt.cla()
+      img3.seek(0)
+      return send_file(img3, mimetype='image/png')
+
+   def nullity_matrix(self):
+      data = self.dataset
+      null_dict = {}
+      for key, value in data.items():
+         # if value.dtype == 'int64' or value.dtype =='float64':
+         null_dict[key] = data[key].count()
+      new_data=pd.Series(null_dict)
+      fig=plt.figure(figsize=(15,8))
+      plt.title(f"Nullity matrix", fontsize = 20)
+      ax =sns.barplot(x=new_data.index, y=new_data.values, palette='summer')
+   
+   
+      for i in ax.containers:
+         ax.bar_label(i,)
+      # plt.bar(range(len(new_data)), new_data.values, align='center')
+      # plt.xticks(range(len(new_data)), new_data.index.values, size='small')
+      canvas = FigureCanvas(fig)
+      img= io.BytesIO()
+      fig.savefig(img)
+      # plt.cla()
+      img.seek(0)
+      return send_file(img, mimetype='image/png')
--- application_routes/routes.py
+++ application_routes/routes.py
@@ -1,15 +1,31 @@
 
+from dataclasses import dataclass
 from pydoc import describe
+from tkinter import Canvas
 from turtle import shape
 import pandas as pd
-from flask import Blueprint, redirect,render_template,flash,request, session, url_for
+from flask import Blueprint, redirect,render_template,flash,request, send_file, session, url_for, Response
 from werkzeug.utils import secure_filename
 from application_operations.application_operations import application_operations
 import os
-
 import datetime;
+from application_logging.logger import App_Logger
+import matplotlib
+matplotlib.use('Agg')
+# import matplotlib
+# matplotlib.use('Agg')
+# from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+# from matplotlib.figure import Figure
+# import io
+# import seaborn as sns
+# import matplotlib.pyplot as plt 
+# import numpy as np
+# import pandas as pd
+
+
 
 application = Blueprint('application', __name__)
+
 
 
 @application.route("/overview", methods=['GET','POST'])
@@ -33,8 +49,66 @@ def column_wise_details():
 
 @application.route("/correlations")
 def correlation():
-    application_process= application_operations()
-    ploting_png=application_process.plot_png()
+    
+    return render_template("correlation.html" )
 
-    print(type(ploting_png))
-    return render_template("correlation.html")
+# @application.route("/visualise")
+# def plot_png():
+      
+#     fig = Figure()
+#     axis = fig.add_subplot(1, 1, 1)
+#     xs = np.random.rand(100)
+#     ys = np.random.rand(100)
+#     axis.plot(xs, ys)
+#     output = io.BytesIO()
+#     fig.savefig(output)
+#     output.seek(0)
+#     FigureCanvas(fig).print_png(output)
+#     return send_file(output, mimetype='image/png')
+
+
+@application.route('/pearson')
+def plot_pearson_heatmap():
+   
+    # application_process=application_operations()
+    # dataset = application_process.return_dataframe(session['filename'])
+    # fig = plt.figure(figsize=(8,8))
+    # plt.title("Heatmap of Pearson's Correlation", fontsize = 20) # title with fontsize 20
+    # plt.xlabel('Years', fontsize = 5) # x-axis label with fontsize 15
+    # plt.ylabel('Monthes', fontsize = 5) # y-axis label with fontsize 15
+    # print(session['filename'])
+    # sns.heatmap(dataset.corr('pearson'), cmap="Blues", annot=True)
+    # canvas = FigureCanvas(fig)
+    # img= io.BytesIO()
+    # fig.savefig(img)
+    # img.seek(0)
+    # return send_file(img, mimetype='image/png')
+
+    application_process = application_operations()
+    application_process.import_csv(session['filename'])
+    return application_process.pearson_correlation()
+
+@application.route('/spearman')
+def plot_spearman_heatmap():
+    application_process = application_operations()
+    application_process.import_csv(session['filename'])
+    return application_process.spearman_correlation()
+
+# @application.route('/kendall')
+# def plot_kendall_heatmap():
+#     application_process = application_operations()
+#     application_process.import_csv(session['filename'])
+#     return application_process.kendall_correlation()
+
+
+@application.route('/mising-values')
+def missing_values():
+    application_process = application_operations()
+    return render_template('missing_values.html')
+    # return render_template('reports.html')
+
+@application.route('/nullity-matrix')
+def nullity_matrix():
+    application_process = application_operations()
+    application_process.import_csv(session['filename'])
+    return application_process.nullity_matrix()
